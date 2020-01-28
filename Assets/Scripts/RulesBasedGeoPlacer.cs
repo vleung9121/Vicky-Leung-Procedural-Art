df42f71ed2b4bd36a18b156021bda8176d27410a
Vicky Leung <vleung@computerart.net>
2020-01-28 14:54:00 -0500

    week 3 classwork

---
+++ Assets/Scripts/RulesBasedGeoPlacer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RulesBasedGeoPlacer : MonoBehaviour
+{
+    public GameObject thing;
+
+    public Vector3 position;
+    public Vector3 scale = Vector3.one;
+    public Vector3 rotation;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        thing.transform.position = position;
+        thing.transform.localScale = scale;
+        thing.transform.eulerAngles = rotation;
+    }
+}
