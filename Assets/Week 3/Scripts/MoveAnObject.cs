51ee2fde163952a3f7a8181bdee56a5e2ddae203
admin <admin@admins-iMac.fios-router.home>
2020-02-03 21:04:49 -0500

    homework3

---
+++ Assets/Week 3/Scripts/MoveAnObject.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAnObject : MonoBehaviour
+{
+    public Vector3 position;
+    public Vector3 rotation;
+    public Vector3 scale = Vector3.one;
+    public float currentTime;
+    int rotateX = 0;
+    int rotateY = 0;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        currentTime = Time.time;
+        rotateX += 2;
+        rotateY += 5;
+        
+        Vector3 jiggle = new Vector3(
+            position.x = Mathf.Sin(currentTime*10)*5,
+            position.y = Mathf.Sin(currentTime)*8, 
+            position.z = Mathf.Cos(currentTime*10)*5   );
+        this.transform.position = jiggle;
+        this.transform.Rotate(rotateX, rotateY, 0);
+    }
+}
